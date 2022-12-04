a81deb38e8b502a6c2cfebfb7e5bee763bc98379
Junhyeok Heo <106042161+Heoveloper@users.noreply.github.com>
2022-12-04 22:37:31 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {};
+        answer = new int[num_list.length];
+        
+        for (int i = 0, j = num_list.length-1; j >= 0; i++, j--) {
+            answer[i] = num_list[j];
+        }
+        
+        return answer;
+    }
+}
