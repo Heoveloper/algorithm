70ccbba31b6ce0734bfe4504590278d423320317
Junhyeok Heo <106042161+Heoveloper@users.noreply.github.com>
2022-12-04 17:20:21 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for (int i = 0; i < 100; i++) {
+            if (n%7 == 0) {
+                answer = n/7;
+            } else {
+                answer = n/7+1;
+            }
+        }
+        
+        return answer;
+    }
+}
