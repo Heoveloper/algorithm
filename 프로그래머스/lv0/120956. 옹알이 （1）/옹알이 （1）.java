4a6f963b9cbc7b6578650f9cbd5004782b8cb598
Junhyeok Heo <106042161+Heoveloper@users.noreply.github.com>
2022-12-18 15:29:40 +0900

    [level 0] Title: 옹알이 (1), Time: 0.18 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,20 @@
+import java.util.regex.Pattern;
+
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        String[] cooing = {"aya", "ye", "woo", "ma"};
+        
+        for (int i = 0; i < babbling.length; i++) {
+            for (int j = 0; j < cooing.length; j++) {
+                if (babbling[i].contains(cooing[j])) {
+                    babbling[i] = babbling[i].replace(cooing[j], "b");
+                }
+            }
+            String pattern = "^[b-b]*$";
+            if (Pattern.matches(pattern, babbling[i])) answer++;
+        }
+        
+        return answer;
+    }
+}
